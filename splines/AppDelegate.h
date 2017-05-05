165c394f8706370e9edd83b40ce37cf9ede524f6
Oluwapelumi <OPelumi@Oluwapelumis-MacBook-Pro.local>
2017-05-05 11:06:11 +0100

    Initial Commit

---
+++ splines/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  splines
+//
+//  Created by Oluwapelumi on 5/5/17.
+//  Copyright © 2017 Oluwapelumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ splines/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  splines
+//
+//  Created by Oluwapelumi on 5/5/17.
+//  Copyright © 2017 Oluwapelumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
