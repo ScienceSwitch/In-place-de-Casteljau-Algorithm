c39868ea2cc92c518ddb2e3219612800c90bbd26
Science Switch <scienceswitch@gmail.com>
2017-07-22 22:46:00 +0100

    Handled interface orientation and added prompt on navigation bar

---
+++ splines/CPoint.h
@@ -0,0 +1,13 @@
+//
+//  CPoint.h
+//  splines
+//
+//  Created by Oluwapelumi on 7/22/17.
+//  Copyright © 2017 Oluwapelumi. All rights reserved.
+//
+
+#ifndef CPoint_h
+#define CPoint_h
+
+
+#endif /* CPoint_h */
---
+++ splines/CPoint.m
@@ -0,0 +1,9 @@
+//
+//  CPoint.m
+//  splines
+//
+//  Created by Oluwapelumi on 7/22/17.
+//  Copyright © 2017 Oluwapelumi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
